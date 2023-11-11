18992df8a36b3d6c96a01bd683407931465ba6c9
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-11-11 10:42:55 +0500

    add code in datatype file

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js node as newer version
+
+let Age = 34            // Number datatypes
+let Name = ""           // String datatypes
+let result = true      // Boolean datatype
+let studentID;         // Undefined datatype
+let number = null         // Null datatype
+
+// Primitive data types
+
+
+
+// number        =>>  2 to power of 53    ( 2 ki power 53 tak number add ho sktey hen otherwise dusra datatype use kro)
+// string        =>>  ""                  ( jo double quotes main aye usay string datatype kehtay hen)
+// boolean       =>>  True / False        ( true or false keliye boolean datatype use krtay hen)
+// null          =>>  Standalone value    ( empty value in digit jisay koi value assign na ki ho )
+// underfined    =>>  Not assigned value  ( esi value jisay datatype diya ho pr usko value assign na ki ho)
+// symbol        =>>  Unique              ( jab ap components bnatay hein , us main sabsy unique dekhney keliye)
+
+console.log(typeof Name)              // string datatype
+console.log(typeof result)            // boolean datatype
+console.log(typeof number)            // object datatype
+console.log(typeof studentID)         // undefined datatype    
+
+
+
