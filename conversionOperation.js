b6f1235903393c4f16af38d55f511fd48bd9a080
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-11-11 16:44:57 +0500

    modify in converionfile

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,9 +1,25 @@
-// let score = "33abc"
-let score = null
-// let score = undefined
+/* Example 1
 
-// console.log(typeof score)  
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "Sham"
+
+console.log(typeof score)
 
 let valueInNumber = Number (score)
 console.log( typeof valueInNumber);
 console.log(valueInNumber);
+ 
+*/
+
+// Example 2  
+
+
+let IsLoggedIn = 1
+
+
+let BoleanLoggedIn = Boolean (IsLoggedIn)
+console.log( typeof BoleanLoggedIn);
+console.log(BoleanLoggedIn);
