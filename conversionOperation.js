6253a23526c9851ce745a4790a0cccad66237ff3
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-11-11 14:51:31 +0500

    Add conversionOperation file

---
+++ conversionOperation.js
@@ -0,0 +1,9 @@
+// let score = "33abc"
+let score = null
+// let score = undefined
+
+// console.log(typeof score)  
+
+let valueInNumber = Number (score)
+console.log( typeof valueInNumber);
+console.log(valueInNumber);
