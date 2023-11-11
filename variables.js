9f0abae5344624481392eb88f39b6d36c50e942a
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-11-11 10:05:10 +0500

    Add Code in variables file

---
+++ variables.js
@@ -0,0 +1,7 @@
+const StudentId = 12345
+let StudentName = "Sham"
+let StudentEmail = "iamjatt57@gmail.com"
+let StudentPassword = "123456"
+let StudentState;
+
+console.table([StudentId, StudentName, StudentEmail, StudentPassword, StudentState])
