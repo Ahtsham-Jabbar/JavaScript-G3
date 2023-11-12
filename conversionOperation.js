7e8cc3170618f1ecd798674db680fa92b067d5f7
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-11-12 20:23:56 +0500

    chnage in CO file

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,25 +1,67 @@
-/* Example 1
+// *********************** CONVERSIONS *********************
 
-// let score = "33abc"
-// let score = null
-// let score = undefined
-// let score = true
+// //  Example 1
+
+// // let score = "33abc"
+// // let score = null
+// // let score = undefined
+// // let score = true
 // let score = "Sham"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber = Number (score)
-console.log( typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number (score)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
  
-*/
-
-// Example 2  
-
-let loggedIn = 1
-
-let BooleanLoggedIn = BOOLEAN (loggedIn)
-console.log(BooleanLoggedIn);
-console.log( typeof BooleanLoggedIn);
 
 
+// // Example 2  
+
+// let loggedIn = 1
+// // let loggedIn = 0
+
+// let BooleanLoggedIn = Boolean(loggedIn)
+// console.log(typeof BooleanLoggedIn);
+// console.log(BooleanLoggedIn);
+
+// // Example 3  
+
+// let SomeNumber = 50
+// // let SomeNumber = "Sham"
+// // let SomeNumber = ""
+// // let SomeNumber = true
+
+// let StringNumber = String(SomeNumber)
+// console.log(typeof StringNumber);
+// console.log(StringNumber);
+
+// *********************** OPERATIONS ********************* //
+
+// Ex 1
+
+// console.log(2+2)
+// console.log("2"+2)
+// console.log("2"+"2")
+
+// Ex 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// Ex 3
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Ex 4
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+
+// ===   When we use triple euqal sign then the compiler compile code strictly and do not give result if type of both datatypes was not same 
