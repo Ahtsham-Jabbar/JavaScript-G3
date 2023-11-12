af46097b11ec3e4374503e83b511cf4e0488eef7
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-11-12 20:53:54 +0500

    Add file dataTypes_Summary

---
+++ datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive DataTypes
+
+// 7 types of datatypes  ( string, Number, Boolean, null, undefined, Symbol, BigInt)
+
+
+const score = 100
+const scoreValue = 100.3
+const IsLoggedIn = false
+const Temperature = null
+let userEmail;
+
+const BigNumber = 1234567890
+
+const id = Symbol('123')
+const Anotherid = Symbol('123')
+
+console.log(id == Anotherid);
+
+
+
+// Non primitive or Rference DataTypes
+
+// 3 types of datatypes  ( Objects, Array, Functions)
+
+// Arrays example 
+const heros = ["Spiderman", "CaptainAmerica", "Hulk"];
+
+// Object example 
+let myobj = {
+    name: "sham",
+    age: 21,
+}
+
+// Functions example 
+
+const MyFunction = function () {
+    console.log("hello Wolrd");
+}
+
+//  console.log(typeof Temperature);
+
+
